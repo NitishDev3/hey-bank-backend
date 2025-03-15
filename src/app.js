9d740bfc6f213953da5c68d876ce55a7c284ea37
nitishdev3 <nitishsahunisi3@gmail.com>
2025-03-15 09:33:16 +0530

    CORS policy changes

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const transactionRouter = require("./router/transactionRouter");
 const app = express();
 
 // middlewares
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+app.use(cors({ origin: "https://customer-online-banking.vercel.app/login", credentials: true }));
 app.use(express.json())
 app.use(cookieParser());
 
